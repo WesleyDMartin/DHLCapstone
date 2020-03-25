6290c1bdb1e5ac6f19dbe9faaac79032b429fece
Wes <wdmartincc@gmail.com>
2020-03-25 11:30:37 -0400

    Added live updating

--- AdminPortal/src/components/AdminPage.vue
+++ AdminPortal/src/components/AdminPage.vue
@@ -18,7 +18,12 @@
           <v-card color="#ededed">
             <v-img :src="cultures.src" class="white--text align-end" height="200px">
               <v-card-title v-text="cultures.title"></v-card-title>
-              <div v-for="(item, index) in cultures.data" :key="item.id">
+              <div
+                class="question"
+                v-for="(item, index) in cultures.data"
+                :key="item.id"
+                v-on:click="updateQuestions(item.name)"
+              >
                 {{index + 1}}.
                 {{item.name}}
               </div>
@@ -27,18 +32,23 @@
           <v-card color="#ededed">
             <v-img :src="questions.src" class="white--text align-end" height="200px">
               <v-card-title v-text="questions.title"></v-card-title>
-              <div v-for="(item, index) in questions.data" :key="item.id">
+              <div
+                class="question"
+                v-for="(item, index) in questions.data"
+                :key="item.id"
+                v-on:click="updateResponse(item.answer)"
+              >
                 {{index + 1}}.
                 {{item.value}}
               </div>
             </v-img>
           </v-card>
-          <v-card color="#ededed">
-            <v-img :src="responses.src" class="white--text align-end" height="200px">
+          <v-card color="#ededed" height="500px">
+            <v-img :src="responses.src" class="white--text align-end">
               <v-card-title v-text="responses.title"></v-card-title>
-              <div v-for="(item, index) in responses.data" :key="item.id">
-                {{index + 1}}.
-                {{item.answer}}
+              <div class="answer">{{responses.data}}
+              <youtube :video-id="O7dq_wo5eto" :player-vars="{autoplay: 1}"></youtube>
+
               </div>
             </v-img>
           </v-card>
@@ -51,6 +61,7 @@
 <script>
 import axios from "axios";
 export default {
+  el: "#app",
   data: () => ({
     cultures: {
       title: "Supported Culture",
@@ -68,16 +79,33 @@ export default {
       title: "Video Responses",
       src: "https://cdn.vuetifyjs.com/",
       flex: 12,
-      data: []
-    }
+      data: ""
+    },
+    test: "test"
   }),
+  methods: {
+    updateQuestions(culture) {
+      axios
+        .get("http://192.168.0.117:3000/questions?culture=" + culture)
+        .then(response => {
+          this.questions.data = response.data;
+          if (response.data[0]) {
+            this.updateResponse(response.data[0].answer)
+          }
+          else {
+            this.responses.data = ""
+          }
+        });
+    },
+    updateResponse(answer) {
+      console.log(answer);
+      this.responses.data = answer;
+    }
+  },
   beforeMount() {
-    axios.get("http://192.168.0.117:3000/questions").then(response => {
-      this.questions.data = response.data;
-      this.responses.data = response.data;
-      axios.get("http://192.168.0.117:3000/cultures").then(response => {
-        this.cultures.data = response.data;
-      });
+    axios.get("http://192.168.0.117:3000/cultures").then(response => {
+      this.cultures.data = response.data;
+      this.questions.data = this.updateQuestions(response.data[0].name)
     });
   }
 };
@@ -87,5 +115,14 @@ export default {
 title {
   margin: 0;
   color: red;
+}
+
+.question {
+  cursor: pointer;
+  margin: 10px;
+}
+
+.answer {
+  margin: 10px;
 }
 </style>
--- AdminPortal/src/main.js
+++ AdminPortal/src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import VueAxios from './plugins/axios'
+import VueYouTubeEmbed from 'vue-youtube-embed'
 var cors = require('cors')
 Vue.use(cors)
 Vue.use(VueAxios)
+Vue.use(VueYouTubeEmbed)
 
 Vue.config.productionTip = false
 
