776780f6837b9e05812a9d8109d467e9a081d8f2
Wes <wdmartincc@gmail.com>
2020-04-19 13:08:35 -0400

    Update technology details

--- AdminPortal/src/components/ProjectPage/ProjectPage.js
+++ AdminPortal/src/components/ProjectPage/ProjectPage.js
@@ -42,19 +42,9 @@ export default {
       ],
       technologies: [
         {
-          tech: "Oculus",
-          description: "Used for Virtual Reality",
-          use: "It is how we provide a fully immersive experience to the users",
-          links: [{
-            value: "More Info",
-            url: "https://www.google.com"
-          }],
-          image: require("../../assets/shots/Controls.png")
-        },
-        {
           tech: "Unity",
-          description: "Used to build all sorts of applications",
-          use: "Unity is how we built the code base and combined all aspects of our platform",
+          description: "A cross platform game engine and development platform",
+          use: "Unity is how we built the code base and combined all aspects of our platform. We used it to create the 3D world where the guide can answer questions. Unity integrates well with the Oculus platform, and as it is C# based, we were easily able to integrate our main application with the Rails API, Google's APIs, and the TensorFlow service.",
           links: [{
             value: "More Info",
             url: "https://www.google.com"
@@ -62,9 +52,19 @@ export default {
           image: require("../../assets/shots/UnityBTS.png")
         },
         {
+          tech: "Oculus",
+          description: "Used to create Virtual Reality worlds",
+          use: "Using the Oculus platform, we were able to create a truly immersive experience for our platforms users. The Oculus VR headset allows users to stand in front of the guide and ask them questions directly, and get both spoken and written responses back. The Oculus is the ideal way for a user to experience the 360 videos which our platform supports, allowing the user to feel like they are IN the place where the video was recorded.",
+          links: [{
+            value: "More Info",
+            url: "https://www.google.com"
+          }],
+          image: require("../../assets/shots/Controls.png")
+        },
+        {
           tech: "TensorFlow",
-          description: "Used to build all sorts of applications",
-          use: "Unity is how we built the code base and combined all aspects of our platform",
+          description: "An end to end Machine Learning platform",
+          use: "TensorFlow is the platform we used to train the sentence encoder, which in turn is how we matched user questions to the most likely question and answer. E.g. A user asks 'What is a wedding like where you are form', and TensorFlow could match on 'What are marriage traditions like in your culture', or 'Do people like to get married where you live' (Assuming either of those questions had been added to the dashboard)",
           links: [{
             value: "More Info",
             url: "https://www.google.com"
@@ -72,9 +72,9 @@ export default {
           image: require("../../assets/shots/Controls.png")
         },
         {
-          tech: "Google Speech to Text and Text to Speech",
-          description: "Used to build all sorts of applications",
-          use: "Unity is how we built the code base and combined all aspects of our platform",
+          tech: "Google Speech to Text API",
+          description: "Google's Speech to Text and Text to Speech APIs are what we used to convert between spoken word and text",
+          use: "User's questions are submitted to the API, and the text response is then sent to the Tensor Flow service. We also use it to generate the audio clips spoken by the guide in the Virtual Reality world",
           links: [{
             value: "More Info",
             url: "https://www.google.com"
@@ -82,9 +82,9 @@ export default {
           image: require("../../assets/shots/Controls.png")
         },
         {
-          tech: "Rails",
-          description: "Used to build all sorts of applications",
-          use: "Unity is how we built the code base and combined all aspects of our platform",
+          tech: "Ruby/Rails",
+          description: "Used to build RESTful APIs and single page web apps",
+          use: "We used Ruby on Rails to build the API which provide the supporte cultures, questions, and answers to the Virtual Reality environment. The content is submitted to the API Via the Administrative Portal web app",
           links: [{
             value: "More Info",
             url: "https://www.google.com"
@@ -93,8 +93,8 @@ export default {
         },
         {
           tech: "Vue",
-          description: "Used to build all sorts of applications",
-          use: "Unity is how we built the code base and combined all aspects of our platform",
+          description: "Used to build snappy user interfaces and single page web apps",
+          use: "We used Vue to build the Administrative Panel for our application. We structured our front end around this framework, as well as all of the applications promotional web pages.",
           links: [{
             value: "More Info",
             url: "https://www.google.com"
@@ -104,32 +104,38 @@ export default {
       ],
       people: [
         {
-          tech: "Wes",
-          description: "Used for Virtual Reality",
-          use: "It is how we provide a fully immersive experience to the users",
+          name: "Wes",
+          role: "Wes was the Project Lead, and was primarily responsible for the Unity and Oculus development, which included ingegration with Google APIs and the TensorFlow project complete by Ricardo. Wes was also responsible for the backend of the Administrative Portal API.",
           links: [{
             value: "LinkedIn",
             url: "https://www.linkedin.com/in/wesley-martin/"
+          }, {
+            value: "GitHub",
+            url: "https://github.com/WesleyDMartin"
           }],
           image: "https://media-exp1.licdn.com/dms/image/C5603AQFics_xolPzfg/profile-displayphoto-shrink_200_200/0?e=1592438400&v=beta&t=vyC36VhylZgk7V_6Gsb2Dvq1aRE9SiwWb5MzgwjV03Y"
         },
         {
-          tech: "Humaira",
-          description: "Used to build all sorts of applications",
-          use: "Unity is how we built the code base and combined all aspects of our platform",
+          name: "Humaira",
+          role: "Humaira was the Team Manager. She helped keep us on track and on schedule. Her main role was as a cultural researcher to complete a sample supported culture for our application. She create the set of questions and answers for the Bangladeshi culture. She also was responsible for getting the admin portal and sign in page set up.",
           links: [{
             value: "LinkedIn",
             url: "https://www.linkedin.com/in/hsiddiqa/"
+          }, {
+            value: "GitHub",
+            url: "https://github.com/hsiddiqa"
           }],
           image: "https://media-exp1.licdn.com/dms/image/C4E03AQEo1xOagBQ8ag/profile-displayphoto-shrink_800_800/0?e=1592438400&v=beta&t=Jf5dUJ2SY0n1Ga8lJl9WhWMa32eis_T4gDq3_pq8z2c"
         },
         {
-          tech: "Ricardo",
-          description: "Used to build all sorts of applications",
-          use: "Unity is how we built the code base and combined all aspects of our platform",
+          name: "Ricardo",
+          role: "Ricardo was primarily responsible for setting up and training the models used in Tensor Flow. He set it up to properly match incoming questions to the bank of exisiting questions",
           links: [{
             value: "LinkedIn",
             url: "https://www.linkedin.com/in/ricardo-mohammed/"
+          }, {
+            value: "GitHub",
+            url: "https://github.com/Fractalbroccoli"
           }],
           image: "https://media-exp1.licdn.com/dms/image/C4D03AQGnXoMXPOqLnA/profile-displayphoto-shrink_200_200/0?e=1592438400&v=beta&t=MnB9kbFvy89kXYHKl6TT05PcrsP-UDz3qaDibJuezDQ"
         }
